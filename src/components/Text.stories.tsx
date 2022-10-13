d21a9bf2f815415c34a9c0b1998591e7f977152d
Cleber <cleber.r.sudul@gmail.com>
2022-10-13 10:54:56 -0300

    Component Text

--- src/components/Text.stories.tsx
+++ src/components/Text.stories.tsx
@@ -10,13 +10,24 @@ export default {
 } as Meta<TextProps>
 
 export const Default: StoryObj<TextProps> = {}
+
 export const Small: StoryObj<TextProps> = {
   args: {
     size: 'sm'
   }
 }
+
 export const Large: StoryObj<TextProps> = {
   args: {
     size: 'lg'
+  }
+}
+
+export const CustomComponent: StoryObj<TextProps> = {
+  args: {
+    asChild: true,
+    children: (
+      <p>Testando</p>
+    )
   }
 }
